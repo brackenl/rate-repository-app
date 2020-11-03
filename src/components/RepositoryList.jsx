44fa52d620121a3e49248a9426c686de014d1210
William Bracken <liam.bracken@gmail.com>
2020-11-03 15:58:40 +0000

    10.24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,80 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import { useLazyQuery } from "@apollo/react-hooks";
+import RNPickerSelect from "react-native-picker-select";
+import { Searchbar } from "react-native-paper";
+import { useDebounce } from "use-debounce";
 
 import { GET_REPOSITORIES } from "../graphql/queries";
 
 import RepositoryListContainer from "./RepositoryListContainer";
 
-const RepositoryList = () => {
-  const { data } = useQuery(GET_REPOSITORIES);
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 14,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    color: "black",
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderRadius: 8,
+    color: "black",
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
 
-  useQuery(GET_REPOSITORIES, {
-    fetchPolicy: "cache-and-network",
-    // Other options
+const RepositoryList = () => {
+  let variables = { orderBy: "RATING_AVERAGE", orderDirection: "DESC" };
+  const [getRepos, { data }] = useLazyQuery(GET_REPOSITORIES, {
+    variables: variables,
   });
+
+  const [sort, setSort] = useState("oldest");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedSearchQuery] = useDebounce(searchQuery, 500);
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  useEffect(() => {
+    console.log(sort, searchQuery);
+    switch (sort) {
+      case "oldest":
+        variables = {
+          orderBy: "CREATED_AT",
+          orderDirection: "DESC",
+          searchKeyword: debouncedSearchQuery,
+        };
+        break;
+      case "latest":
+        variables = {
+          orderBy: "CREATED_AT",
+          orderDirection: "ASC",
+          searchKeyword: debouncedSearchQuery,
+        };
+        break;
+      case "highestRated":
+        variables = {
+          orderBy: "RATING_AVERAGE",
+          orderDirection: "DESC",
+          searchKeyword: debouncedSearchQuery,
+        };
+        break;
+      case "lowestRated":
+        variables = {
+          orderBy: "RATING_AVERAGE",
+          orderDirection: "ASC",
+          searchKeyword: debouncedSearchQuery,
+        };
+        break;
+    }
+    console.log("variables: ", variables);
+    getRepos({ variables: variables });
+  }, [sort, debouncedSearchQuery]);
 
   // Get the nodes from the edges array
   let repositoryNodes = [];
@@ -19,7 +82,30 @@ const RepositoryList = () => {
     repositoryNodes = data.repositories.edges.map((edge) => edge.node);
   }
 
-  return <RepositoryListContainer repositories={repositoryNodes} />;
+  return (
+    <>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+        style={{ marginHorizontal: 10, marginTop: 5 }}
+      />
+      <RNPickerSelect
+        onValueChange={(value) => {
+          setSort(value);
+        }}
+        items={[
+          { label: "Latest repositiories", value: "latest" },
+          { label: "Oldest repositiories", value: "oldest" },
+          { label: "Highest rated repositiories", value: "highestRated" },
+          { label: "Lowest rated repositiories", value: "lowestRated" },
+        ]}
+        placeholder={{ label: "Sort by...", value: "latest" }}
+        style={pickerSelectStyles}
+      />
+      <RepositoryListContainer repositories={repositoryNodes} />
+    </>
+  );
 };
 
 export default RepositoryList;
